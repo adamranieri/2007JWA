335564c8b3f68d99458fe040cea11d2b90486514
Adam_Ranieri <adam.ranieri@revature.com>
2020-07-06 18:19:59 -0400

    added first day material

---
+++ JavaDay1/src/main/java/dev/ranieri/conventions/JavaConvention.java
@@ -0,0 +1,31 @@
+package dev.ranieri.conventions;
+
+// classes follow PascalCase Start with a capital and every word following is capitalized
+// Person
+// FireFighter
+// MoneyTranserService
+public class JavaConvention {
+
+	
+	// the golden rule of naming is someone who is not you should understand what that variable or method means
+
+	
+	//variables follow camel case
+	String fname;
+	String last_name; // Java does not use snake case except for enums
+	int age;
+
+	
+	String emailAddress;
+	String mailingAddress;
+
+	
+	// commonly for booleans we start with is
+	boolean isAlive;
+	
+	// methods are also camel case
+	public void sayHello(){
+		
+	}	
+	String $$ = "Hello";
+}
